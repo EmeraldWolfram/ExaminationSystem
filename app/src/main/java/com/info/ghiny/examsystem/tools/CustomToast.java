664e5812d19bcae4011681a79a61a9776222ef6e
EmeraldWolfram <stevenfgy@yahoo.com>
2016-06-06 16:31:06 +0800

    Modified CustomToast to show Custom Layout of Toast

--- app/src/main/java/com/info/ghiny/examsystem/tools/CustomToast.java
+++ app/src/main/java/com/info/ghiny/examsystem/tools/CustomToast.java
@@ -1,17 +1,31 @@
 package com.info.ghiny.examsystem.tools;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.content.Context;
+import android.test.ActivityTestCase;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.info.ghiny.examsystem.R;
 
 /**
  * Created by GhinY on 23/05/2016.
  */
 public class CustomToast {
     Toast message;
-    Context context;
+    Activity activity;
 
-    public CustomToast(Context context){
-        this.context = context;
+    /*public CustomToast(Activity activity){
+        this.activity = activity;
+    }*/
+
+    public CustomToast(Activity activity){
+        this.activity = activity;
+
     }
 
     public void showMessage(String textMessage){
@@ -19,8 +33,21 @@ public class CustomToast {
             //If there is a Toast message displaying
             message.cancel();
         }
-        message = Toast.makeText(context, textMessage, Toast.LENGTH_LONG);
+        message = new Toast(activity);
+        message.setDuration(Toast.LENGTH_LONG);
+        LayoutInflater toastInflater = activity.getLayoutInflater();
+        View toastView      = toastInflater.inflate(R.layout.toast_message_layout,
+                                (ViewGroup) activity.findViewById(R.id.toastLayout));
+        TextView msgView    = (TextView) toastView.findViewById(R.id.toastTxt);
+        msgView.setText(textMessage);
+        message.setView(toastView);
         message.show();
     }
+
+    //public void showMessageWithCondition(String textMessage, boolean condition){
+    //    if(condition){
+
+    //    }
+    //}
 
 }
