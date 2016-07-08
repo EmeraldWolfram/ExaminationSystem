e52ea7581103b59202b82f9686e4e69249c9473f
EmeraldWolfram <stevenfgy@yahoo.com>
2016-07-08 09:53:04 +0800

    Added dialog cancel to close the dialog for default

--- app/src/main/java/com/info/ghiny/examsystem/tools/ProcessException.java
+++ app/src/main/java/com/info/ghiny/examsystem/tools/ProcessException.java
@@ -49,6 +49,7 @@ public class ProcessException extends Exception {
             listener = new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
                 }
             };
         }
